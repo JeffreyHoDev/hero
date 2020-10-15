b0a6d51aae33d09c3c35f3b5646686a0e37c70c3
JeffreyHoDev <JeffreyHoDev@gmail.com>
2020-10-15 19:37:18 +0800

    display images of heroes

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // Proxy
-var proxyUrl = 'https://corsreverseproxyserver.herokuapp.com/'; 
+var proxyUrl = 'https://corsreverseproxyserver.herokuapp.com/';
+
+// Get element
+const grid = document.getElementById('image-grid');
 
 // Heroes API
 async function CallHeroAPI(){
-    const response = await fetch(proxyUrl + 'https://superheroapi.com/api/774532146722883/1')
-    const data = await response.json();
-    console.log(data)
+    for(var i=0; i < 20; i++) {
+        try{
+            const response = await fetch(proxyUrl + `https://superheroapi.com/api/774532146722883/${Math.round(Math.random()*600)}`)
+            const data = await response.json();
+            var Card = document.createElement('img');
+            Card.setAttribute('src', data.image.url);
+            var imageCard = document.createElement('div');
+            imageCard.setAttribute('class', 'image-card');
+            imageCard.appendChild(Card);
+            grid.appendChild(imageCard)
+            console.log(data)
+        }catch(error){
+            console.log(error)
+        }
+
+    }
+    
+    
 }
 
 CallHeroAPI()
